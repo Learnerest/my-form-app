e61840899eca72a31240dd5d6ce71f0401eb1921
Fatih Kurt <fatihkurt@Fatih-MacBook-Air.local>
2020-10-09 16:41:21 +0300

    Updates

---
+++ precache-manifest.50d8ac05e56364867c831ba2c96fe26b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d4633d23da58c20f4526b1e1df3ad1",
+    "url": "/my-form-app/index.html"
+  },
+  {
+    "revision": "0e26e579431e4a6ff2cd",
+    "url": "/my-form-app/static/css/main.b6385f68.chunk.css"
+  },
+  {
+    "revision": "6be27309062b02b6edc3",
+    "url": "/my-form-app/static/js/2.e7218778.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-form-app/static/js/2.e7218778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e26e579431e4a6ff2cd",
+    "url": "/my-form-app/static/js/main.ad03dc42.chunk.js"
+  },
+  {
+    "revision": "2a832ec54f578dcf4d19",
+    "url": "/my-form-app/static/js/runtime-main.8cdc8e73.js"
+  }
+]);
